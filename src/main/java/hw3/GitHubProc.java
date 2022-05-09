87a6997e697225fa72a8f548b05a0d0c16c9b2f9
aleeckman <aleeckman@gmail.com>
2022-05-08 23:52:10 -0700

    First Resume Upload

--- src/main/java/hw3/GitHubProc.java
+++ src/main/java/hw3/GitHubProc.java
@@ -1,35 +1,71 @@
 package hw3;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.averagingDouble;
+import static java.util.stream.Collectors.summingLong;
+
 public class GitHubProc {
   public static Long getWordCount(Stream<GitHubComment> stream, String word) {
-    return null;
+    long wc = stream.parallel()
+      .map(
+        (t) -> Stream.of(Util.getWords(t.body()))
+          .filter(w -> w.equals(word))
+          .count()
+      )
+      .reduce((long)0, (x, y) -> x + y);
+
+    return wc;
   }
 
   public static Map<String, Long> getPerProjectCount(Stream<GitHubComment> stream) {
-    return null;
+    Map<String, Long> map = new HashMap<String, Long>();
+    map = stream.parallel()
+      .collect(groupingBy(Util::getProject, counting()));
+
+    return map;
   }
 
   public static Map<String, Long> getAuthorActivity(Stream<GitHubComment> stream) {
-    return null;
+    Map<String, Long> map = new HashMap<String, Long>();
+
+    map = stream.parallel()
+      .collect(groupingBy(GitHubComment::author, counting()));
+
+    return map;
   }
 
   public static Map<String, Long> getCommentUrlAuthorCount(Stream<GitHubComment> stream) {
-    return null;
+    Map<String, Long> map = new HashMap<String, Long>();
+    map = filterCommentsWithUrl(stream).parallel()
+      .collect(groupingBy(GitHubComment::author, counting()));
+    
+    return map;
   }
 
   public static Stream<GitHubComment> filterCommentsWithUrl(Stream<GitHubComment> comments) {
-    return null;
+    return comments.parallel()
+      .filter((t) -> t.body().contains("http://") || t.body().contains("https://"));
   }
 
   public static Map<String, Double> getAuthorAverageVerbosity(Stream<GitHubComment> stream) {
-    return null;
+    Map<String, Double> map = new HashMap<String, Double>();
+    map = stream.parallel()
+      .collect(groupingBy(GitHubComment::author, averagingDouble((GitHubComment t) -> Util.getWords(t.body()).length)));
+
+    return map;
   }
 
-  public static Map<String, Map<String, Long>> getAuthorWordCountPerProject(
-      Stream<GitHubComment> stream, String word) {
-    return null;
+  public static Map<String, Map<String, Long>> getAuthorWordCountPerProject(Stream<GitHubComment> stream, String word) {
+
+    return stream.parallel().collect(groupingBy(Util::getProject, 
+      groupingBy(GitHubComment::author, 
+        summingLong((t) -> Stream.of(Util.getWords(t.body()))
+          .filter(words -> words.equals(word))
+          .count()))));
   }
 }
--- src/test/java/hw3/StudentTest.java
+++ src/test/java/hw3/StudentTest.java
@@ -1,12 +1,97 @@
 package hw3;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 public class StudentTest {
+    Stream<GitHubComment> getTestData() {
+        return Stream.of(
+            new GitHubComment(
+                "22422968",
+                "a9f297a8f06d98cdaf9246a776ddea3d8e5b1634",
+                "https://github.com/javaproject/javaproject/pull/590#discussion_r22422968",
+                "Crichton",
+                "2015-01-02T21:10:32Z",
+                "Version 1.0, I don't remember enough Java. May contain a few errors"),
+            new GitHubComment(
+                "22423062",
+                "c6343604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22423062",
+                "Tolstoy",
+                "2015-01-02T21:13:33Z",
+                "Not quite sure about the Draw part of method getDrawMetric. Can it just be getMetrics()?"),
+            new GitHubComment(
+                "22422664",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422662",
+                "Hemmingway",
+                "2015-01-02T21:00:14Z",
+                "This is contains errors : printStackElement prints out the entire stack trace when button is pressed."),
+            new GitHubComment(
+                "22425855",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422663",
+                "Hemmingway",
+                "2015-01-02T21:00:14Z",
+                "This is a project really makes C++ look really good. I create less errors in C++."),
+            new GitHubComment(
+                "22422664",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422662",
+                "Tolstoy",
+                "2015-01-02T21:00:14Z",
+                "toString() override, reference: https://www.stackoverflow.com"),
+            new GitHubComment(
+                "22422664",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422662",
+                "Crichton",
+                "2015-01-02T21:00:14Z",
+                "I don't want to work on this project anymore, there's too many errors"),
+            new GitHubComment(
+                "22422664",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422662",
+                "Crichton",
+                "2015-01-02T21:00:14Z",
+                "Okay i fixed the code, check out this playlist btw: https://www.spotify.com/playlist/random"),
+            new GitHubComment(
+                "22422664",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422662",
+                "Hemmingway",
+                "2015-01-02T21:00:14Z",
+                "No Crichton, you just added more errors"),
+            new GitHubComment(
+                "22422664",
+                "c6333604d6532d85cd531dcc7336d4626cbeba2e",
+                "https://github.com/javaproject/javaproject/pull/376#discussion_r22422662",
+                "Crichton",
+                "2015-01-02T21:00:14Z",
+                "I am finding a new team, yall should look at this: http://www.howtocode.com"));
+      }
+
+
     @Test
-    public void studentShouldTest() { // TODO: rename test name to something meaningful
-        assertFalse(true); // TODO: implement a non-trivial test
+    public void shouldCountWordUsageManyAuthorsSingularProjectTest() { 
+        var testMap = GitHubProc.getAuthorWordCountPerProject(getTestData(), "errors");
+        var expectedMap = new HashMap<String, Map<String, Long>>();
+
+        expectedMap.put(
+            "javaproject/javaproject",
+            new HashMap<String, Long>() {
+              {
+                put("Crichton", 2L);
+                put("Tolstoy", 0L);
+                put("Hemmingway", 3L);
+              }
+            });
+
+        assertEquals(testMap, expectedMap);
     }
 }
